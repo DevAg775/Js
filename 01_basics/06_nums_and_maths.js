ed30e72d2328b424917e50dda2082d06e2867d82
DevAg775 <148446133+DevAg775@users.noreply.github.com>
2025-05-04 05:09:29 +0000

    Maths Imp.

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -14,3 +14,29 @@ console.log(balance.toFixed(2));
 const otherNumber = 23.8966
 
 console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++  Maths ++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.min(4 , 5, 8, 9));
+console.log(Math.max(4 , 5, 8, 9));
+
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min)) // to generate values in betweeen 10 and 20 
